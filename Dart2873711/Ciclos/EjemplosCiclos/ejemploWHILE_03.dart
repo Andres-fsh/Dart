e2600cc807c56fb3eecc28244ea8da2f6a218e67
Andres <andresfsh7@gmail.com>
2024-05-07 11:58:52 -0500

    ejemplos while

---
+++ Dart2873711/Ciclos/EjemplosCiclos/ejemploWHILE_03.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  /*
+  Desarrollar un algoritmo que lea n números positivos, determine y muetre el promedio de los n números positivos
+  */
+  print("Ingrese numeros enteros positivos");
+  int num;
+  double average;
+  
+}
