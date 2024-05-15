390c2875aa074ae7d22d84108e2f1f794fe2f2fb
Andres <andresfsh7@gmail.com>
2024-05-14 23:29:59 -0500

    fin FOR

---
+++ Dart2873711/Ciclos/EjerciciosGuia4/WHILE/ejercicioWHILE_02.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  //ANDRÉS FELIPE SÁNCHEZ H
+  /*
+  En una empresa se requiere calcular el salario semanal de cada uno de los n obreros que laboran en ella. El salario se obtiene de la sig. forma:
+  Si el obrero trabaja 40 horas o menos se le paga $20 por hora
+  Si trabaja más de 40 horas se le paga $20 por cada una de las primeras 40 horas y $25 por cada hora extra.
+  */
+  int valorHora=20, horaExtra=25, cantTrabajadores= 5;
+  double sueldoSemanal;
+}
